44d4d5a58f893cf9a397c166031c7dc02671f056
Jesse Kelly <kellyjesse881@gmail.com>
2020-06-26 13:37:33 -0500

    feat(site): added footer

--- site/project.clj
+++ site/project.clj
@@ -8,7 +8,10 @@
 
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.773"]
-                 [org.clojure/core.async  "0.4.500"]]
+                 [org.clojure/core.async  "0.4.500"]
+                 [reagent "1.0.0-alpha2"]
+                 [stylefy "2.2.1"]
+                 ]
 
   :plugins [[lein-figwheel "0.5.20"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
--- site/src/site/core.cljs
+++ site/src/site/core.cljs
@@ -1,17 +1,81 @@
 (ns site.core
-    (:require ))
+  (:require [goog.dom :as gdom]
+            [reagent.dom :as dom]
+            [stylefy.core :as stylefy :refer [use-style]]))
 
-(enable-console-print!)
+(def top-page-style {:height "55vh"
+                     :background-repeat "no-repeat"
+                     :background-position "center bottom"
+                     :background-color "#F2AA8085"
+                     :background-image "url('http://seanhalpin.io/assets/img/content/home/masthead/land.svg')"})
 
-(println "This text is printed from src/site/core.cljs. Go ahead and edit it and see reloading in action.")
+(stylefy/init)
 
-;; define your app data so that it doesn't get over-written on reload
+(defn site-title [text]
+  [:h1.siteTitle text])
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defn page [& children]
+  [:section children])
 
+(defn site-footer []
+  [:footer.siteFooter
+   [:span "Footer"]
+   ])
 
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(defn page-title [text]
+  [:h2.pageTitle text])
+
+(defn page-sub-title [text]
+  [:h3.pageSubTitle text])
+
+(defn top-page []
+  [:section (use-style top-page-style)
+     [site-title "Jesse Kelly"]
+  ])
+
+(defn hire-me-page []
+  [:section (use-style {:background-color "#F2AA8085" :height "45vh"})
+   [page
+    [page-title "Hire Me!"]
+    ]
+   ])
+
+(defn footer []
+  [:section (use-style {:margin-top "20px" :text-align "center"})
+   [:small "Built using Clojurescript!"]]
+  )
+
+(defn app []
+  [:div
+   [:title "Jesse Kelly"]
+   [top-page]
+   [page
+       [page-title "Profile"]
+   ]
+
+   [page
+       [page-title "Education"]
+   ]
+
+   [page
+       [page-title "Experience"]
+   ]
+
+   [page
+       [page-title "Abilities"]
+       [page-sub-title "Skills"]
+       [page-sub-title "Languages"]
+       [page-sub-title "Tools"]
+   ]
+
+   [page
+       [page-title "Projects"]
+   ]
+
+   [hire-me-page]
+   [footer]
+
+   ]
+  )
+
+(dom/render [app] (gdom/getElement "app"))
